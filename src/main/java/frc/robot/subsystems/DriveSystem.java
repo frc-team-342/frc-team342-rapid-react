6e38ebef52cc4e3442129520ba713d795a24d2e8
VZamora5 <victoriazamora007@gmail.com>
2022-01-20 17:39:00 -0500

    Made mecanum drivesystem and its drive command

--- src/main/java/frc/robot/subsystems/DriveSystem.java
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -4,11 +4,46 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSystem extends SubsystemBase {
+
+  private MecanumDrive mecanumDrive;
+  private CANSparkMax frontLeft;
+  private CANSparkMax backLeft;
+  private CANSparkMax frontRight;
+  private CANSparkMax backRight;
+
+  private boolean fieldOriented = true;
+  private AHRS navx;
+
   /** Creates a new DriveSystem. */
-  public DriveSystem() {}
+  public DriveSystem() 
+  {
+    frontLeft = new CANSparkMax(1, MotorType.kBrushless);
+    backLeft = new CANSparkMax(2, MotorType.kBrushless);
+    frontRight = new CANSparkMax(3, MotorType.kBrushless);
+    backRight = new CANSparkMax(4, MotorType.kBrushless);
+
+    mecanumDrive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
+    navx = new AHRS();
+  }
+
+  public void drive(double xVelocity, double yVelocity, double rotationVelocity) {
+
+    if (fieldOriented) 
+    {
+      mecanumDrive.driveCartesian(yVelocity, xVelocity, rotationVelocity, -navx.getAngle());
+    } else {
+      mecanumDrive.driveCartesian(yVelocity, xVelocity, rotationVelocity);
+    }
+  }
 
   @Override
   public void periodic() {
