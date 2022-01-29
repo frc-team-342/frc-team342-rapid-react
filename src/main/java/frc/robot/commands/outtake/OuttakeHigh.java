b2464d15fb59aca84644e20e3713eede6544d3f9
VZamora5 <victoriazamora007@gmail.com>
2022-01-29 11:13:56 -0500

    Added outtake commands for high and low

---
+++ src/main/java/frc/robot/commands/outtake/OuttakeHigh.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.outtake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.OuttakeSubsystem;
+
+public class OuttakeHigh extends CommandBase {
+  private OuttakeSubsystem subsystem;
+
+  /** Creates a new OuttakeHigh. */
+  public OuttakeHigh(OuttakeSubsystem subsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    subsystem.shootHigh();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    subsystem.stopShooter();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
