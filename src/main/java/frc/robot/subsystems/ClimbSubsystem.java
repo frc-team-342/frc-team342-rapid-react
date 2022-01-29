745579d022ccfbbd657453483cb6d8492a765414
reganh342 <rehuggins14@gmail.com>
2022-01-29 09:51:39 -0500

    built limit switches for climb

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -7,18 +7,23 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
 
   private TalonSRX climbMotor1;
   private TalonSRX climbMotor2;
+  private DigitalInput limitSwitch1;
+  private DigitalInput limitSwitch2;
 
   /** Creates a new ClimbSubsystem. */
   public ClimbSubsystem() {
 
     TalonSRX climbMotor1 = new TalonSRX(8);
     TalonSRX climbMotor2 = new TalonSRX(8);
+    DigitalInput limitSwitch1 = new DigitalInput(0);
+    DigitalInput limitSwitch2 = new DigitalInput(1);
 
   }
   
@@ -27,11 +32,20 @@ public class ClimbSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
+  
+  
 
   public void activateClimb(){
 
-    climbMotor1.set(ControlMode.PercentOutput, 1);
-    climbMotor2.set(ControlMode.PercentOutput, 1);
+
+    if (!limitSwitch1.get() && !limitSwitch2.get()){
+      climbMotor1.set(ControlMode.PercentOutput, 1);
+      climbMotor2.set(ControlMode.PercentOutput, 1);
+    }else{
+      deactivateClimb();
+    }
+
+
   }
 
   public void deactivateClimb(){
@@ -39,5 +53,6 @@ public class ClimbSubsystem extends SubsystemBase {
     climbMotor1.set(ControlMode.PercentOutput, 0);
     climbMotor2.set(ControlMode.PercentOutput, 0);
   }
+
 
 }
