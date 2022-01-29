41fbab0bc6b8f9915f4c66f07913761916cd699e
VZamora5 <victoriazamora007@gmail.com>
2022-01-29 10:46:06 -0500

    Documented photonvision methods

--- src/main/java/frc/robot/vision/PhotonVision.java
+++ src/main/java/frc/robot/vision/PhotonVision.java
@@ -60,30 +60,54 @@ public class PhotonVision implements Sendable {
         return driverMode;
     }
 
+    /**
+     * Sets the driver mode 
+     * 
+     * @param mode True; Driver mode is selected False; Driver mode not selected
+     */
     public void setDriverMode(boolean mode) {
         driverMode = mode;
         msCam.setDriverMode(driverMode);
     }
 
+    /**
+     * Sets driver mode to the opposite of its current mode
+     */
     public void toggleDriverMode() {
         driverMode = !driverMode;
         setDriverMode(driverMode);
     }
     
+    /**
+     * Sets pipeline mode
+     * @param mode {@link PipelineMode}
+     */
     public void setPipeline(PipelineMode mode) {
         this.pipeline = mode;
         msCam.setPipelineIndex(pipeline.pipelineIndex);
     }
 
+    /**
+     * Gets current pipeline mode
+     * @return Pipeline mode
+     */
     public PipelineMode getPipeline() {
         return pipeline;
     }
 
+    /**
+     * Checks to see if photonvision has any targets
+     * @return True if targets detected; False if no targets detected
+     */
     public boolean hasTargets() {
         PhotonPipelineResult result = msCam.getLatestResult();
         return result.hasTargets();
     }
 
+    /**
+     * Gets horizontal offset from crosshair to target. 
+     * @return Angle measure of offset
+     */
     public double getHorizontalOffset() {
         PhotonPipelineResult result = msCam.getLatestResult();
         
@@ -95,6 +119,10 @@ public class PhotonVision implements Sendable {
        return Double.NaN;
     }
 
+    /**
+     * Gets vertical offset from crosshair to target.
+     * @return Angle measure of offset 
+     */
     public double getVerticalOffset() {
         PhotonPipelineResult result = msCam.getLatestResult();
 
@@ -106,6 +134,10 @@ public class PhotonVision implements Sendable {
         return Double.NaN;
     }
 
+    /**
+     * Represents movement needed to get to target
+     * @return Trajectory
+     */
     public Transform2d transformToTarget() {
         PhotonPipelineResult result = msCam.getLatestResult();
         
