1bf059a900b104f5684a24cd7993c1a5004e1d40
VZamora5 <victoriazamora007@gmail.com>
2022-02-05 09:42:25 -0500

    Added deploy command for intake

--- src/main/java/frc/robot/commands/Intake/Deploy.java
+++ src/main/java/frc/robot/commands/Intake/Deploy.java
@@ -5,11 +5,20 @@
 package frc.robot.commands.Intake;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public class Deploy extends CommandBase {
+
+  private IntakeSubsystem subsystem;
+
+
   /** Creates a new Deploy. */
-  public Deploy() {
+  public Deploy(IntakeSubsystem subsystem) {
+
+    this.subsystem = subsystem;
+
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -18,11 +27,18 @@ public class Deploy extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    subsystem.deployIntake();
+    subsystem.intakeCargo();
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    
+  }
 
   // Returns true when the command should end.
   @Override
