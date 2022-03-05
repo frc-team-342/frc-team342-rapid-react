70c9ae0bce90f0192e5d1ed5abc765835030a018
ctorek <ctorek@protonmail.ch>
2022-03-05 11:37:33 -0500

    Added a command for stage two climb

---
+++ src/main/java/frc/robot/commands/climb/StageTwoClimb.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.climb;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ClimbSubsystem;
+
+public class StageTwoClimb extends CommandBase {
+  private ClimbSubsystem climb;
+  private XboxController xbox;
+
+  /** Creates a new StageTwoClimb. */
+  public StageTwoClimb(ClimbSubsystem climb, XboxController xbox) {
+    this.climb = climb;
+    this.xbox = xbox;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(climb);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    climb.rotateClimb(xbox.getRightX());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    climb.stopClimbRotate();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
