96853803192285163bb60a6faf12014a0b700ee8
ctorek <ctorek@protonmail.ch>
2022-05-05 18:44:55 -0400

    started velocity drive cmd

---
+++ src/main/java/frc/robot/commands/drive/DriveVelocity.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.drive;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSystem;
+
+public class DriveVelocity extends CommandBase {
+  private DriveSystem driveSystem;
+  private Joystick joystick;
+
+  /** Creates a new DriveVelocity. */
+  public DriveVelocity(DriveSystem driveSystem, Joystick joystick) {
+    this.driveSystem = driveSystem;
+    this.joystick = joystick;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveSystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
